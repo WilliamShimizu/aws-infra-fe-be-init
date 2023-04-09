b3ce59a4ba293a51da6cb5fc628709ad9793454b
shimizu <will.conrardy@gmail.com>
2023-04-09 21:12:36 +0900

    Adding gitignore, packaging script, and deployment workflow

---
+++ packaging.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+LAMBDA_BUCKET=aws-infra-fe-be-init-lambda-us-west-2-dev
+
+for lambda_dir in backend/*/; do
+    # Skip the common directory
+    if [[ $lambda_dir == *"common"* ]]; then
+      continue
+    fi
+
+    lambda_name=$(basename $lambda_dir)
+
+    # Package the Lambda code
+    cd $lambda_dir
+    cp -r ../common .
+    python -m pip install -r requirements.txt -t .
+    zip -r ../../${lambda_name}.zip *
+    cd ../..
+
+    # Check to see if the zip exists in the bucket before we copy it.
+    zip_exists=$(aws s3 ls s3://${LAMBDA_BUCKET}/${lambda_name}.zip | wc -l)
+
+    # Upload the Lambda code to S3
+    aws s3 cp ${lambda_name}.zip s3://${LAMBDA_BUCKET}/${lambda_name}.zip
+
+    # Update the Lambda function's code only if the .zip file already existed.
+    # Deployment is such that the first time, we don't execute this call because infra will not have been deployed yet.
+    # We will upload the zip, then execute the cfn.
+    if [ $zip_exists -eq 1 ]; then
+        aws lambda update-function-code --function-name ${lambda_name} --s3-bucket $LAMBDA_BUCKET --s3-key ${lambda_name}.zip
+    fi
+
+done
