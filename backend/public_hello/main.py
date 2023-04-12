6fe413cb1dda1258d547797c5340325d421c725a
shimizu <will.conrardy@gmail.com>
2023-04-12 09:53:43 +0900

    Testing to see if path appending to lambda is actually needed

--- backend/public_hello/main.py
+++ backend/public_hello/main.py
@@ -1,11 +1,4 @@
-import sys
-import os
 import json
-
-# Add the path to the shared code directory to sys.path
-common_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if common_path not in sys.path:
-    sys.path.append(common_path)
 
 from common.shared import get_message
 
